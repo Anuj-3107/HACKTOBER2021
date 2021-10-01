035751926f50c10b190cd04781e1fce522fc88e4
DeepakParihar221 <68172840+DeepakParihar221@users.noreply.github.com>
2021-10-01 08:09:53 +0530

    dekh lena anuj
    done

---
+++ minIndex.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class minIndex {
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int a[] = new int[n];
+        int b[] = new int[n];
+        for(int i=0;i<n;++i)
+        {    
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+        }
+        
+        for(int i=0;i<n;++i)
+        {
+            for(int j=b[i];j<n;++j)
+            {
+                if()
+            }
+        }
+
+        sc.close();
+    }
+}
+
+
